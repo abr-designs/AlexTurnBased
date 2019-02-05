5dd7a9997c28a4bae1be835a7e491ed5447a9e4c
Alex Bedard-Reid <alexbedardreid@gmail.com>
2019-02-05 17:27:01 -0500

    Base Sprites

---
+++ TurnBased_CaseStudy/Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class CharacterBase : MonoBehaviour
+{
+    [SerializeField, ReadOnly]
+    protected STATE currentState;
+    
+    [Required, SerializeField]
+    protected CharacterStats stats;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
---
+++ TurnBased_CaseStudy/Assets/Scripts/Enums/STATE.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum STATE : int
+{
+    AI,
+    WAITING,
+    STUNNED,
+    DONE,
+    DEAD
+}
